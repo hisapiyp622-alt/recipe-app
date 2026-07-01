ed27492af4de1dfaa69a40912b744267e2e8a216
hisapiyp622-alt <hisapiyp622@gmail.com>
2026-07-01 17:57:30 +0900

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// Firebaseプロジェクト: recipe-box (recipe-box-bd642)
+const firebaseConfig = {
+  apiKey: "AIzaSyDkGmaumzmTeFyRdEqOaxD4rItFzl2bn3w",
+  authDomain: "recipe-box-bd642.firebaseapp.com",
+  projectId: "recipe-box-bd642",
+  storageBucket: "recipe-box-bd642.firebasestorage.app",
+  messagingSenderId: "179572630427",
+  appId: "1:179572630427:web:1189138bed0730ba9d773e"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
